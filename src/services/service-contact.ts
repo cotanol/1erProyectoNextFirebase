fdacf504698f63631e27b1c7ba76213101c44110
cotanol <salcedomoralesfranciscojose@gmail.com>
2025-02-09 17:22:32 -0500

    v1.3.1 Pequeño error

--- src/services/service-contact.ts
+++ src/services/service-contact.ts
@@ -5,7 +5,7 @@ import { addDoc, collection } from "firebase/firestore";
 export async function addContact(contact: Contact) {
   try {
     const collectionName = collection(db, "contacts");
-    const { id: _id, ...contactWithoutId } = contact;
+    const { id, ...contactWithoutId } = contact;
     await addDoc(collectionName, contactWithoutId);
   } catch (error) {
     console.error("Error adding document: ", error);
