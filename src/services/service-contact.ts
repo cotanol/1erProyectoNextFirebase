e1ba8ec52a392e24f81963f94f11381b2e4b266a
cotanol <salcedomoralesfranciscojose@gmail.com>
2025-02-09 17:33:31 -0500

    Uso en desarrollo de id Contact

--- src/services/service-contact.ts
+++ src/services/service-contact.ts
@@ -5,7 +5,10 @@ import { addDoc, collection } from "firebase/firestore";
 export async function addContact(contact: Contact) {
   try {
     const collectionName = collection(db, "contacts");
-    const { id: _, ...contactWithoutId } = contact;
+    const { id, ...contactWithoutId } = contact;
+    if (process.env.NODE_ENV === "development") {
+      console.log("Ignoring id:", id); // Solo se imprime en desarrollo
+    }
     await addDoc(collectionName, contactWithoutId);
   } catch (error) {
     console.error("Error adding document: ", error);
